6333f651ec7302e45f9e4372337f2b05b7ce724b
sphatak21 <siddphatak@gmail.com>
2018-11-12 10:50:57 -0500

    first upload 3 down

--- js/problem-set-4.js
+++ js/problem-set-4.js
@@ -5,7 +5,8 @@
 function hello() {
 
   // WRITE YOUR EXERCISE 1 CODE HERE
-
+  var div=document.getElementById("output1");
+  div.innerHTML=("Hello, AP Computer Science Principles!");
   ////////////////// DO NOT MODIFY
   check("hello"); // DO NOT MODIFY
   ////////////////// DO NOT MODIFY
@@ -22,6 +23,8 @@ function helloAgain() {
   //////////// DO NOT MODIFY      in response to your prompt.
 
   // WRITE YOUR EXERCISE 2 CODE HERE
+  name=prompt("Please Enter Your Name")
+  document.getElementById("output2").innerHTML=("Hello, " + name + "!");
 
   ///////////////////////////// DO NOT MODIFY
   check("helloAgain", name); // DO NOT MODIFY
@@ -37,7 +40,8 @@ function celsius() {
   //////////////////////////////////////////////////////// DO NOT MODIFY
   let cels = ((Math.random() * 1001) - 100).toFixed(2); // DO NOT MODIFY
   //////////////////////////////////////////////////////// DO NOT MODIFY
-
+  var fahrenheit = ((cels * 9/5) + 32).toFixed(2);
+  document.getElementById("output3").innerHTML=(cels+" degrees Celsius equals "+fahrenheit + " degrees Fahrenheit.");
   // The above code generates a random number between -100 and 1000
   // (inclusive), and rounds this value to 2 decimal places.
 
@@ -62,6 +66,8 @@ function fahrenheit() {
   // (inclusive), and rounds this value to 2 decimal places.
 
   // WRITE YOUR EXERCISE 4 CODE HERE
+  var celsius=((fahr-32)*(5/9)).toFixed(2);
+  document.getElementById("output4").innerHTML=(fahr+" degrees Fahrenheit equals "+celsius+ " degrees Celsius.");
 
   ///////////////////////////// DO NOT MODIFY
   check("fahrenheit", fahr); // DO NOT MODIFY
@@ -86,7 +92,24 @@ function inches() {
   ////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 5 CODE HERE
-
+  var miles = 0;
+  var yard = 0;
+  var feet=0;
+  while (inches => 12) {
+    if(inches > 63360) {
+      inches = inches - 63360;
+      miles=miles+1;
+    }
+    if (63369>inches>36) {
+      inches = inches-36;
+      yard=yard+1;
+    }
+    if (36>inches>12) {
+      inches=inches-12;
+      feet=feet+1;
+    }
+  }
+  document.getElementById("output5").innerHTML=(miles+"miles"+yard+"yards"+feet+"feet"+inches+"inches");
   ////////////////////////// DO NOT MODIFY
   check("inches", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
